24ab4cd2b50e679070f1036c4ddb1546ada3e901
Erick Díaz Quiñones <erickillodq@hotmail.com>
2025-10-09 20:26:57 -0600

    Mejora Registro Usuarios

--- LoginDeAbarrotech/RegistroUsuarios.xaml.cs
+++ LoginDeAbarrotech/RegistroUsuarios.xaml.cs
@@ -57,53 +57,21 @@ namespace LoginDeAbarrotech
                 ct_Usuario.Focus();
                 return;
             }
-            else { 
-
-            string usuario;
-            string nombre;
-            string contrasena;
-            string apellido_paterno;
-            string apellido_materno;
-            string preparatoria;
-            usuario = ct_Usuario.Text;
-            nombre = ct_Nombre.Text;
-            contrasena = ct_Contrasena.Password;
-            apellido_paterno = ct_ApellidoPaterno.Text;
-            apellido_materno = ct_ApellidoMaterno.Text;
-            preparatoria = ct_PreparatoriaOrigen.Text;
-
-            usuarioGenerico.usuario = usuario;
-            usuarioGenerico.nombre = nombre;
-            usuarioGenerico.contrasena = contrasena;
-            usuarioGenerico.apellido_paterno = apellido_paterno;
-            usuarioGenerico.apellido_materno= apellido_materno;
-            usuarioGenerico.preparatoria_origen = preparatoria;
-
-            ConexionBD conexion = new ConexionBD();
-            bool aux = conexion.ingresar_usuarios(usuarioGenerico);
-            if (aux)
-            {
-                MessageBox.Show("Usuario registrado con exito");
-                this.Close();
-            }
             else
             {
-                MessageBox.Show("Error al registrar el usuario");
-            }
-
-            bool aux = conexion.ingresar_usuarios(usuarioGenerico);
-            if (aux)
-            {
-                MessageBox.Show("Usuario registrado con exito");
-                this.Close();
-            }
-            else
-            {
-                MessageBox.Show("Error al registrar el usuario");
-            }
-            }
+                bool aux = conexion.ingresar_usuarios(usuarioGenerico);
+                if (aux)
+                {
+                    MessageBox.Show("Usuario registrado con exito");
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Error al registrar el usuario");
+                }
 
 
+            }
         }
 
         private void btn_Cancelar_Click(object sender, RoutedEventArgs e)
